a338a6718891bbeb62d0bed52fd97ebf986efcef
Vladimir Lalith <lalith.vemula@gmail.com>
2025-07-21 23:53:00 +0530

    Made a small .env mistake

--- client/src/api/auth.ts
+++ client/src/api/auth.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 // Define your backend API URL
 //for deploypush
-const API_URL = 'import.meta.env.VITE_API_BASE_URL'; // Make sure this matches your backend
+const API_URL = import.meta.env.VITE_API_BASE_URL; // Make sure this matches your backend
 
 interface AuthResponse {
   _id: string;
